c2122541da37f7611240e90d0fd6cf52e925bb9f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-01-17 12:10:30 +0100

    Remove verification tokens usage

--- db/schema/user.mjs
+++ db/schema/user.mjs
@@ -1,17 +1,9 @@
 import { sql } from "drizzle-orm";
-import {
-  boolean,
-  integer,
-  pgTable,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: varchar({ length: 255 }).notNull().unique(),
   verified: boolean().default(sql`FALSE`),
-  verificationToken: varchar({ length: 255 }),
-  verificationTokenExpires: timestamp(),
   password: varchar({ length: 255 }).notNull(),
 });
